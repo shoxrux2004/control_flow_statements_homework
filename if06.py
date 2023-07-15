9474ddbdf3f31e710d194d37bc5c93026f0e6b7d
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-07-15 12:55:50 +0500

    positiv and negativ

--- if06.py
+++ if06.py
@@ -13,4 +13,11 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
-    return
+    if a>0 and b>0 and c>0:
+        return "there are a lot of positive numbers"
+    if (a>0 and b>0 and c<0) or (a>0 and b<0 and c>0) or (a<0 and b>0 and c>0):
+        return "there are a lot of positive numbers"
+    else:
+        return "there are a lot of negative numbers"
+print(main(-2,4,1))
+print(main(3,-3,-6))
