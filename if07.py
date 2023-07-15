000c0d55802c6b580051f31dc91c961d33cfe7bf
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-07-15 13:05:15 +0500

    musbat,toq,nol

--- if07.py
+++ if07.py
@@ -12,4 +12,15 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a>0 and a%2==1:
+        return "positive odd number"
+    if a>0 and a%2==0:
+        return "positive even number"
+    if a<0 and a%2==1:
+        return "negative odd number"
+    if a<0 and a%2==0:
+        return "negative even number"
+    if a==0:
+        return "the number is zero"
+print(main(57))
+print(main(-24))
