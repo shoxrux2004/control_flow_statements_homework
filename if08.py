d1aceedd0b91d03a9c4ee3ed4338b77e98853c7d
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-07-15 13:12:41 +0500

    ikki va uch xonali son

--- if08.py
+++ if08.py
@@ -11,4 +11,13 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a>9 and a<100 and a%2==1:
+        return "two-digit odd number"
+    if a>9 and a<100 and a%2==0:
+        return "two-digit even number"
+    if a>99 and a<1000 and a%2==1:
+        return "three-digit odd number"
+    if a>99 and a<1000 and a%2==0:
+        return "three-digit even number"
+print(main(57))
+print(main(242))
